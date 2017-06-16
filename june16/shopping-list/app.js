5fa0e595c781522a8c8a713203bd004ea5dd0826
52lions06 <mckelvey.william@yahoo.com>
2017-06-16 13:28:07 -0500

    This is a test

--- june16/shopping-list/app.js
+++ june16/shopping-list/app.js
@@ -1,8 +1,71 @@
 'usestrict';
 
-console.log('hi');
+//Section 1 Setting up AppState:
 
-add
-check
-uncheck
-remove
+var state = {
+	items: [
+		{name: 'apple', checked: false}; // test
+	]
+};
+
+//Section 2 Modification functions
+//input needs to show once clicking the "return" or "add item" button 
+
+function addItem (state, item){
+	var objectToAdd = {name: item, checked: false};
+	// state.items.push(item);
+    state.items.push(objectToAdd);
+    
+}
+
+//check
+function itemChecked (state, item) {
+	// var bob = items.checked 
+	// 	//.shopping-item__checked or nothing here
+
+ //    state.items.find(function(item) {
+ //    	if (item === .shopping-item__checked) {
+ //    		//do this 
+ //    	}
+ //    	else {
+ //    		//do that
+ //    	}
+ //    })
+
+
+    // is item checked? 
+    // if true, uncheck it
+    // else false, check it
+}
+
+// remove
+function removeItem(state, item) {
+    // grab item from click of delete button and remove from app state
+    state.items.splice(items,1);
+}
+
+// Tests
+console.log(state);
+
+
+//Section 3 Rendering functions 
+function render(state, something) {
+    // add to html
+}
+
+
+//Section 4 Put everything into the DOM - Event listeners
+function handleIt(state) {
+    $('.submit').submit(function(event) {
+        event.preventDefault();
+        addItem(state, $('#shopping-list-entry').val());
+    })
+}
+
+
+
+//Initialize
+// $(function() {
+//     render(parm1, parm2);
+//     handleIt(parm)
+// })
